af6701a82423bbeff4656560e042f0f657484c15
phschiele <phschiele@gmail.com>
2022-11-25 18:55:19 -0800

    add to init

--- dspp/__init__.py
+++ dspp/__init__.py
@@ -1,3 +1,21 @@
+from dspp.atoms import (
+    concave_inf,
+    convex_concave_inner,
+    convex_sup,
+    inner,
+    weighted_log_sum_exp,
+)
 from dspp.cvxpy_integration import extend_cone_canon_methods
+from dspp.problem import MinimizeMaximize, SaddleProblem
+
+__all__ = [
+    "concave_inf",
+    "convex_sup",
+    "convex_concave_inner",
+    "inner",
+    "weighted_log_sum_exp",
+    "MinimizeMaximize",
+    "SaddleProblem",
+]
 
 extend_cone_canon_methods()
