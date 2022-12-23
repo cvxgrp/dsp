306fd2549aac3ae3e5d750ef7e8daa0e234a432d
phschiele <phschiele@gmail.com>
2022-12-23 21:53:30 +0100

    parametrize tests

--- tests/test_weighted_norm2.py
+++ tests/test_weighted_norm2.py
@@ -1,13 +1,15 @@
 import cvxpy as cp
 import numpy as np
+import pytest
 
 import dsp
 from dsp.atoms import weighted_norm2
 
 
-def test_weighted_norm2():
-    x = cp.Variable(2, name="x", nonneg=True)
-    y = cp.Variable(2, name="y", nonneg=True)
+@pytest.mark.parametrize("n", [1, 2, 3, 4, 5])
+def test_weighted_norm2(n):
+    x = cp.Variable(n, name="x", nonneg=True)
+    y = cp.Variable(n, name="y", nonneg=True)
 
     norm = weighted_norm2(x, y)
 
@@ -15,7 +17,10 @@ def test_weighted_norm2():
     prob = dsp.SaddlePointProblem(obj, [y <= 2, x >= 1])
     prob.solve()
 
+    y_expected = np.ones(n) * 2
+    x_expected = np.ones(n) * 1
+
     assert prob.status == "optimal"
-    assert np.isclose(prob.value, 2.0)
-    assert np.allclose(x.value, 1)
-    assert np.allclose(y.value, 2)
+    assert np.isclose(prob.value, np.sqrt(y_expected @ x_expected**2))
+    assert np.allclose(x.value, x_expected)
+    assert np.allclose(y.value, y_expected)
