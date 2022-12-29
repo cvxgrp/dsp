b9cef28d6011104b0f1b35e8a2096729d0df081d
phschiele <phschiele@gmail.com>
2022-12-29 14:51:35 +0100

    add saddle_quad_form to init

--- dsp/__init__.py
+++ dsp/__init__.py
@@ -1,4 +1,11 @@
-from dsp.atoms import inner, saddle_inner, saddle_max, saddle_min, weighted_log_sum_exp
+from dsp.atoms import (
+    inner,
+    saddle_inner,
+    saddle_max,
+    saddle_min,
+    saddle_quad_form,
+    weighted_log_sum_exp,
+)
 from dsp.cvxpy_integration import add_is_dsp, extend_cone_canon_methods
 from dsp.local import LocalVariable
 from dsp.problem import MinimizeMaximize, SaddlePointProblem, is_dsp
@@ -7,6 +14,7 @@ __all__ = [
     "saddle_min",
     "saddle_max",
     "saddle_inner",
+    "saddle_quad_form",
     "inner",
     "weighted_log_sum_exp",
     "MinimizeMaximize",
