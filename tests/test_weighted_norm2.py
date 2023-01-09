5e99fb42a7cad7d6bd1fa9161bdbd0a576bc104c
phschiele <phschiele@gmail.com>
2023-01-09 15:25:24 +0100

    Allow non-affine precompositions for weighted norm

--- tests/test_weighted_norm2.py
+++ tests/test_weighted_norm2.py
@@ -69,24 +69,36 @@ def test_affine_precompositions(n):
     assert np.allclose(y.value, y_expected)
 
 
+@pytest.mark.parametrize("x_val", [-1, 0.1, 1])
+@pytest.mark.parametrize(
+    "x_expr", [lambda x: cp.square(x), lambda x: cp.square(x) - 1, lambda x: cp.exp(x)]
+)
 @pytest.mark.parametrize("n", [1, 2, 3, 4, 5])
-def test_nonaffine_precompositions(n):
+def test_nonaffine_precompositions(x_val, x_expr, n):
 
-    x = cp.Variable(n, name="x", nonneg=True)
-    y = cp.Variable(n, name="y", nonneg=True)
+    x = cp.Variable(n, name="x")
+    y = cp.Variable(n, name="y")
+
+    cvx_expr = x_expr(x)
 
     with pytest.warns(UserWarning, match="Weights are non-positive."):
-        norm = weighted_norm2(cp.exp(x), cp.log(y))
-    assert norm.is_dsp()
+        norm = weighted_norm2(cvx_expr, y)
 
-    obj = dsp.MinimizeMaximize(norm)
-    prob = dsp.SaddlePointProblem(obj, [y <= 2, x >= 1])
-    prob.solve()
+    if cvx_expr.is_affine() or cvx_expr.is_nonneg():
+        assert norm.is_dsp()
 
-    y_expected = np.ones(n) * 2
-    x_expected = np.ones(n) * 1
+        obj = dsp.MinimizeMaximize(norm)
 
-    assert prob.status == "optimal"
-    assert np.isclose(prob.value, np.sqrt(np.log(y_expected) @ np.exp(x_expected) ** 2))
-    assert np.allclose(x.value, x_expected)
-    assert np.allclose(y.value, y_expected)
+        x_range_constr = x >= x_val if x_val > 0 or cvx_expr.is_incr(0) else x <= x_val
+        prob = dsp.SaddlePointProblem(obj, [y <= 2, x_range_constr])
+        prob.solve()
+
+        y_expected = np.ones(n) * 2
+        x_expected = np.ones(n) * x_val
+
+        assert prob.status == "optimal"
+        assert np.isclose(prob.value, np.sqrt(y_expected @ (x_expr(x_expected).value ** 2)))
+        assert np.allclose(x.value, x_expected)
+        assert np.allclose(y.value, y_expected, atol=1e-5)
+    else:
+        assert not norm.is_dsp()
