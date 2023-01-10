d666fb989b600cd3497590f51d911ab8fc71826d
phschiele <phschiele@gmail.com>
2023-01-10 14:43:25 +0100

    add numeric property to SaddleAtoms

--- dsp/atoms.py
+++ dsp/atoms.py
@@ -58,6 +58,10 @@ class SaddleAtom(Atom, ABC):
     def is_dsp(self) -> bool:
         pass
 
+    @abstractmethod
+    def numeric(self, values: list) -> np.ndarray | None:
+        pass
+
     def is_atom_convex(self) -> bool:
         return False
 
@@ -136,6 +140,11 @@ class saddle_inner(SaddleAtom):
         else:
             K_out.concave_expr = lambda x: -self.get_convex_expression()
         return K_out
+
+    def numeric(self, values: list) -> np.ndarray | None:
+        if any([v is None for v in values]):
+            return None
+        return values[0] @ values[1]
 
     def convex_variables(self) -> list[cp.Variable]:
         return self.Fx.variables()
@@ -218,6 +227,12 @@ class weighted_log_sum_exp(SaddleAtom):
         assert exponents.shape == weights.shape or exponents.size == weights.size
 
         super().__init__(exponents, weights)
+
+    def numeric(self, values: list) -> np.ndarray | None:
+        if any([v is None for v in values]):
+            return None
+        x, y = values
+        return np.log(np.sum(y * np.exp(x)))
 
     def is_dsp(self) -> bool:
         x_cvx = self.exponents.is_convex()
@@ -354,6 +369,277 @@ class weighted_log_sum_exp(SaddleAtom):
 
     def is_incr(self, idx: int) -> bool:
         return True  # increasing in both arguments since y nonneg
+
+
+class saddle_quad_form(SaddleAtom):
+    def __init__(self, x: cp.Expression, P: cp.Expression) -> None:
+        assert isinstance(x, cp.Expression)
+        assert isinstance(P, cp.Expression)
+
+        assert x.size == P.shape[0] == P.shape[1]
+
+        self.x = x
+        self.P = P
+
+        super().__init__(x, P)
+
+    def numeric(self, values: list) -> np.ndarray | None:
+        if any(v is None for v in values):
+            return None
+        x, P = values
+        return x.T @ P @ x
+
+    def is_dsp(self) -> bool:
+        return self.P.is_psd() and self.x.is_affine()
+
+    def get_concave_expression(self) -> cp.Expression:
+        x = self.x
+        assert x.value is not None
+        x = np_vec(x.value, order="F")
+        P = self.P
+        return x.T @ P @ x
+
+    def get_convex_expression(self) -> cp.Expression:
+        P = self.P
+        assert P.value is not None
+
+        x = self.x
+        return cp.quad_form(x, P)
+
+    def _get_K_repr(self, local_to_glob: LocalToGlob, switched: bool = False) -> KRepresentation:
+        n = self.x.size
+        F_local = cp.Variable((n, n), name="F_quad_form_local", PSD=True)
+        A = cp.Variable((n + 1, n + 1), name="A_quad_form", PSD=True)
+        t = cp.Variable(name="t_quad_form")
+
+        constraints = [
+            A[:n, :n] == F_local,
+            A[-1, -1] == 1,
+            A[:n, -1] == self.x,
+            A[-1, :n] == self.x.T,
+            t == 0,
+        ]
+
+        B, c = affine_to_canon(self.P, local_to_glob, switched)
+        F_global = cp.Variable(
+            local_to_glob.y_size if not switched else local_to_glob.x_size,
+            name="f_global_saddle_quad_form",
+        )
+        constraints += [F_global == B.T @ cp.vec(F_local)]
+
+        K_repr = KRepresentation(
+            f=F_global,
+            t=t,
+            constraints=constraints,
+            concave_expr=lambda x: self.get_concave_expression(),
+        )
+
+        if switched:
+            K_repr = switch_convex_concave(
+                constraints, F_global, t, self.convex_variables(), local_to_glob
+            )
+            K_repr.concave_expr = lambda x: self.get_convex_expression()
+
+        return K_repr
+
+    def name(self) -> str:
+        return "saddle_quad_form(" + self.x.name() + ", " + self.P.name() + ")"
+
+    def convex_variables(self) -> list[cp.Variable]:
+        return self.x.variables()
+
+    def concave_variables(self) -> list[cp.Variable]:
+        return self.P.variables()
+
+    def shape_from_args(self) -> tuple[int, ...]:
+        return ()
+
+    def sign_from_args(self) -> tuple[bool, bool]:
+        return (True, False)
+
+    def is_incr(self, idx: int) -> bool:
+        return False
+
+    def is_decr(self, idx: int) -> bool:
+        return False
+
+
+class weighted_norm2(SaddleAtom):
+    def __init__(self, x: cp.Expression, y: cp.Expression) -> None:
+        """
+        Implements the function f(x,y) = (sum_i y_i x_i^2)^(1/2) for vectors x and y.
+        The weights, y, must be non-negative. If they are non recognized by
+        cvxpy as nonnegative, an implicit domain constraint is added and a
+        warning is provided.
+        The exponents can be any convex cvxpy expression.
+
+        The conic representation is:
+            (sum_i y_i x_i^2)^(1/2)
+            min. f, t       f @ y + t
+            subject to      f >= 0
+                            t >= 0
+                            sqrt(t*f) >= 0.5 * abs(x)
+        """
+
+        assert isinstance(x, cp.Expression)
+        assert isinstance(y, cp.Expression)
+
+        if not y.is_nonneg():
+            warnings.warn(
+                "Weights are non-positive. The domain of weighted_norm2 is y >="
+                " 0. The implicit constraint y >= 0 will be added to"
+                " the problem."
+            )
+
+        self.concave_composition = not y.is_affine()
+
+        self.x = x
+        self.y = y
+
+        assert len(x.shape) == 1 and len(y.shape) == 1
+        assert x.shape == y.shape
+
+        super().__init__(x, y)
+
+    def numeric(self, values: list) -> np.ndarray | None:
+        if any(v is None for v in values):
+            return None
+        x, y = values
+        return np.sqrt(np.sum(y * x**2))
+
+    def is_dsp(self) -> bool:
+        x_cvx = cp.norm2(self.x).is_convex()
+        y_ccv = self.y.is_concave()
+        return x_cvx and y_ccv
+
+    def _get_K_repr(self, local_to_glob: LocalToGlob, switched: bool) -> KRepresentation:
+        f_local = cp.Variable(self.y.size, name="f_wnorm2")
+        t = cp.Variable(name="t_wnorm2")
+
+        # See https://www2.isye.gatech.edu/~nemirovs/LMCOLN2022Fall.pdf 2.3.5
+        tau = cp.Variable(self.x.size, name="tau")
+        sum_half = (f_local + t) / 2
+        diff_half = (f_local - t) / 2
+        stack = cp.vstack([tau, diff_half])
+
+        constraints = [
+            t >= 0,
+            f_local >= 0,
+            cp.norm2(stack, axis=0) <= sum_half,
+            tau >= 0.5 * cp.abs(self.x),
+        ]
+
+        t_global = cp.Variable(name="t_global")
+        if self.concave_composition:
+            constraints += [t_global == t]
+            f_global = cp.Variable(self.y.size, name="f_global_wnorm2_comp")
+            constraints += [f_global == f_local]
+        else:
+            B, c = affine_to_canon(self.y, local_to_glob, switched)
+            constraints += [t_global == t + f_local @ c]
+            f_global = cp.Variable(
+                local_to_glob.y_size if not switched else local_to_glob.x_size,
+                name="f_global_wnorm2",
+            )
+            constraints += [f_global == B.T @ f_local]
+
+        K_repr = KRepresentation(
+            f=f_global,
+            t=t_global,
+            constraints=constraints,
+        )
+
+        switching_variables = self.x.variables()
+
+        precomp = cp.Variable(self.y.size, name="z_wnorm2") if self.concave_composition else None
+        K_out = (
+            switch_convex_concave(
+                constraints,
+                f_global,
+                t_global,
+                switching_variables,
+                local_to_glob,
+                precomp,
+            )
+            if switched
+            else K_repr
+        )
+
+        if self.concave_composition:
+            if not switched:
+                x_vars_1 = self.convex_variables() if not switched else self.concave_variables()
+                K_switch_1 = switch_convex_concave(
+                    K_out.constraints,
+                    K_out.f,
+                    K_out.t,
+                    x_vars_1,
+                    local_to_glob,
+                    precomp=precomp,
+                )
+                K_switch_1.constraints += [self.y >= precomp]
+
+                # dualize the outer concave x variables if switched
+                x_vars_2 = self.concave_variables() if not switched else self.convex_variables()
+                K_out = switch_convex_concave(
+                    K_switch_1.constraints,
+                    K_switch_1.f,
+                    K_switch_1.t,
+                    x_vars_2,
+                    local_to_glob,
+                )
+
+            else:
+                K_out.constraints += [precomp <= self.y]
+
+        if not self.y.is_nonneg():
+            if switched:
+                K_out.constraints += [self.y >= 0]
+            else:
+                K_out.y_constraints += [self.y >= 0]
+
+        concave_fun = (
+            (lambda x: self.get_concave_expression())
+            if not switched
+            else (
+                lambda x: -self.get_convex_expression()
+                if self.get_convex_expression() is not None
+                else None
+            )
+        )
+        K_out.concave_expr = concave_fun
+
+        return K_out
+
+    def get_concave_expression(self) -> cp.Expression:
+        x = self.x
+        assert x.value is not None
+
+        y = self.y
+
+        return cp.sqrt(y.T @ np.square(x))
+
+    def get_convex_expression(self) -> cp.Expression:
+        y = self.y
+        assert y.value is not None
+
+        x = self.x
+
+        return cp.norm2(x * np.sqrt(y))
+
+    def convex_variables(self) -> list[cp.Variable]:
+        return self.x.variables()
+
+    def concave_variables(self) -> list[cp.Variable]:
+        return self.y.variables()
+
+    def shape_from_args(self) -> tuple[int, ...]:
+        return ()
+
+    def sign_from_args(self) -> tuple[bool, bool]:
+        return (True, False)
+
+    def is_incr(self, idx: int) -> bool:
+        return self.x.is_nonneg()
 
 
 def init_parser_wrapper(
@@ -627,93 +913,6 @@ class saddle_min(SaddleExtremum):
         return self.f.shape
 
 
-class saddle_quad_form(SaddleAtom):
-    def __init__(self, x: cp.Expression, P: cp.Expression) -> None:
-        assert isinstance(x, cp.Expression)
-        assert isinstance(P, cp.Expression)
-
-        assert x.size == P.shape[0] == P.shape[1]
-
-        self.x = x
-        self.P = P
-
-        super().__init__(x, P)
-
-    def is_dsp(self) -> bool:
-        return self.P.is_psd() and self.x.is_affine()
-
-    def get_concave_expression(self) -> cp.Expression:
-        x = self.x
-        assert x.value is not None
-        x = np_vec(x.value, order="F")
-        P = self.P
-        return x.T @ P @ x
-
-    def get_convex_expression(self) -> cp.Expression:
-        P = self.P
-        assert P.value is not None
-
-        x = self.x
-        return cp.quad_form(x, P)
-
-    def _get_K_repr(self, local_to_glob: LocalToGlob, switched: bool = False) -> KRepresentation:
-        n = self.x.size
-        F_local = cp.Variable((n, n), name="F_quad_form_local", PSD=True)
-        A = cp.Variable((n + 1, n + 1), name="A_quad_form", PSD=True)
-        t = cp.Variable(name="t_quad_form")
-
-        constraints = [
-            A[:n, :n] == F_local,
-            A[-1, -1] == 1,
-            A[:n, -1] == self.x,
-            A[-1, :n] == self.x.T,
-            t == 0,
-        ]
-
-        B, c = affine_to_canon(self.P, local_to_glob, switched)
-        F_global = cp.Variable(
-            local_to_glob.y_size if not switched else local_to_glob.x_size,
-            name="f_global_saddle_quad_form",
-        )
-        constraints += [F_global == B.T @ cp.vec(F_local)]
-
-        K_repr = KRepresentation(
-            f=F_global,
-            t=t,
-            constraints=constraints,
-            concave_expr=lambda x: self.get_concave_expression(),
-        )
-
-        if switched:
-            K_repr = switch_convex_concave(
-                constraints, F_global, t, self.convex_variables(), local_to_glob
-            )
-            K_repr.concave_expr = lambda x: self.get_convex_expression()
-
-        return K_repr
-
-    def name(self) -> str:
-        return "saddle_quad_form(" + self.x.name() + ", " + self.P.name() + ")"
-
-    def convex_variables(self) -> list[cp.Variable]:
-        return self.x.variables()
-
-    def concave_variables(self) -> list[cp.Variable]:
-        return self.P.variables()
-
-    def shape_from_args(self) -> tuple[int, ...]:
-        return ()
-
-    def sign_from_args(self) -> tuple[bool, bool]:
-        return (True, False)
-
-    def is_incr(self, idx: int) -> bool:
-        return False
-
-    def is_decr(self, idx: int) -> bool:
-        return False
-
-
 class conjugate(saddle_max):
     r"""f^*(y) = sup_{x\in \dom f} (y^Tx - f(x))"""
 
@@ -734,175 +933,3 @@ class conjugate(saddle_max):
         constraints = [x <= B for x in x_vars] + [x >= -B for x in x_vars]
 
         super().__init__(obj, constraints)
-
-
-class weighted_norm2(SaddleAtom):
-    def __init__(self, x: cp.Expression, y: cp.Expression) -> None:
-        """
-        Implements the function f(x,y) = (sum_i y_i x_i^2)^(1/2) for vectors x and y.
-        The weights, y, must be non-negative. If they are non recognized by
-        cvxpy as nonnegative, an implicit domain constraint is added and a
-        warning is provided.
-        The exponents can be any convex cvxpy expression.
-
-        The conic representation is:
-            (sum_i y_i x_i^2)^(1/2)
-            min. f, t       f @ y + t
-            subject to      f >= 0
-                            t >= 0
-                            sqrt(t*f) >= 0.5 * abs(x)
-        """
-
-        assert isinstance(x, cp.Expression)
-        assert isinstance(y, cp.Expression)
-
-        if not y.is_nonneg():
-            warnings.warn(
-                "Weights are non-positive. The domain of weighted_norm2 is y >="
-                " 0. The implicit constraint y >= 0 will be added to"
-                " the problem."
-            )
-
-        self.concave_composition = not y.is_affine()
-
-        self.x = x
-        self.y = y
-
-        assert len(x.shape) == 1 and len(y.shape) == 1
-        assert x.shape == y.shape
-
-        super().__init__(x, y)
-
-    def is_dsp(self) -> bool:
-        x_cvx = cp.norm2(self.x).is_convex()
-        y_ccv = self.y.is_concave()
-        return x_cvx and y_ccv
-
-    def _get_K_repr(self, local_to_glob: LocalToGlob, switched: bool) -> KRepresentation:
-        f_local = cp.Variable(self.y.size, name="f_wnorm2")
-        t = cp.Variable(name="t_wnorm2")
-
-        # # See https://www2.isye.gatech.edu/~nemirovs/LMCOLN2022Fall.pdf 2.3.5
-        tau = cp.Variable(self.x.size, name="tau")
-        sum_half = (f_local + t) / 2
-        diff_half = (f_local - t) / 2
-        stack = cp.vstack([tau, diff_half])
-
-        constraints = [
-            t >= 0,
-            f_local >= 0,
-            cp.norm2(stack, axis=0) <= sum_half,
-            tau >= 0.5 * cp.abs(self.x),
-        ]
-
-        t_global = cp.Variable(name="t_global")
-        if self.concave_composition:
-            constraints += [t_global == t]
-            f_global = cp.Variable(self.y.size, name="f_global_wnorm2_comp")
-            constraints += [f_global == f_local]
-        else:
-            B, c = affine_to_canon(self.y, local_to_glob, switched)
-            constraints += [t_global == t + f_local @ c]
-            f_global = cp.Variable(
-                local_to_glob.y_size if not switched else local_to_glob.x_size,
-                name="f_global_wnorm2",
-            )
-            constraints += [f_global == B.T @ f_local]
-
-        K_repr = KRepresentation(
-            f=f_global,
-            t=t_global,
-            constraints=constraints,
-        )
-
-        switching_variables = self.x.variables()
-
-        precomp = cp.Variable(self.y.size, name="z_wnorm2") if self.concave_composition else None
-        K_out = (
-            switch_convex_concave(
-                constraints,
-                f_global,
-                t_global,
-                switching_variables,
-                local_to_glob,
-                precomp,
-            )
-            if switched
-            else K_repr
-        )
-
-        if self.concave_composition:
-            if not switched:
-                x_vars_1 = self.convex_variables() if not switched else self.concave_variables()
-                K_switch_1 = switch_convex_concave(
-                    K_out.constraints,
-                    K_out.f,
-                    K_out.t,
-                    x_vars_1,
-                    local_to_glob,
-                    precomp=precomp,
-                )
-                K_switch_1.constraints += [self.y >= precomp]
-
-                # dualize the outer concave x variables if switched
-                x_vars_2 = self.concave_variables() if not switched else self.convex_variables()
-                K_out = switch_convex_concave(
-                    K_switch_1.constraints,
-                    K_switch_1.f,
-                    K_switch_1.t,
-                    x_vars_2,
-                    local_to_glob,
-                )
-
-            else:
-                K_out.constraints += [precomp <= self.y]
-
-        if not self.y.is_nonneg():
-            if switched:
-                K_out.constraints += [self.y >= 0]
-            else:
-                K_out.y_constraints += [self.y >= 0]
-
-        concave_fun = (
-            (lambda x: self.get_concave_expression())
-            if not switched
-            else (
-                lambda x: -self.get_convex_expression()
-                if self.get_convex_expression() is not None
-                else None
-            )
-        )
-        K_out.concave_expr = concave_fun
-
-        return K_out
-
-    def get_concave_expression(self) -> cp.Expression:
-        x = self.x
-        assert x.value is not None
-
-        y = self.y
-
-        return cp.sqrt(y.T @ np.square(x))
-
-    def get_convex_expression(self) -> cp.Expression:
-        y = self.y
-        assert y.value is not None
-
-        x = self.x
-
-        return cp.norm2(x * np.sqrt(y))
-
-    def convex_variables(self) -> list[cp.Variable]:
-        return self.x.variables()
-
-    def concave_variables(self) -> list[cp.Variable]:
-        return self.y.variables()
-
-    def shape_from_args(self) -> tuple[int, ...]:
-        return ()
-
-    def sign_from_args(self) -> tuple[bool, bool]:
-        return (True, False)
-
-    def is_incr(self, idx: int) -> bool:
-        return self.x.is_nonneg()
